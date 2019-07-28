fbc9e0472148301b38aeefc5ceb5d1d5558acecb
Marcelo Gobelli <marcelo@Marcelos-MacBook-Pro.local>
2019-07-28 13:51:37 -0700

    work with leila

--- data/gen/nguyen_nc_2018/20190613-fastas/a_new_fasta.py
+++ data/gen/nguyen_nc_2018/20190613-fastas/a_new_fasta.py
@@ -7,19 +7,24 @@ def group_lines(incoming_lines, separator):
             # If there are no groups at all yet or we see a line that starts a                                           
             # new group, we add a group to th elist.
             groups.append([])
+            #print(groups)
         groups[-1].append(line)
+        #print(line)
     return groups
 
 
 def main():
     with open('chrMchr1-1.fasta.eg') as f:
         groups = group_lines(f, '>')
+        print(groups[1])
     for number, group in enumerate(groups):
         outfile = "output_{}.txt".format(number)
-        print("Writing {} lines to {}".format(len(group), outfile))
+        #print("Writing {} lines to {}".format(len(group), outfile))
+        #print(len(group[0]))
         with open(outfile, 'w') as out:
             out.writelines(group)
             print(''.join(group))
+            
     return 0
 
 if __name__ == '__main__':
--- data/gen/nguyen_nc_2018/20190613-fastas/test_one.py
+++ data/gen/nguyen_nc_2018/20190613-fastas/test_one.py
@@ -1,24 +1,30 @@
 import errno
+import glob
 
-mylines = []
+files = glob.glob('*.fasta.eg')
+x = 1
 
-with open('chrMchr1-1.fasta.eg', 'r') as myfile:
-	for a_line in myfile:
-		if a_line == '>2 chr1:1-X\n':
-			break
-		mylines.append(a_line.rstrip('\n')) 
+#mylines = []
+for file in files:
+	with open(file, 'r') as myfile:
+		mylines = []
+		for a_line in myfile:
+			if a_line == '>2 chr1:1-X\n':
+				break
+			mylines.append(a_line.rstrip('\n')) 
 
-with open('chrMchr1_1_ONLY.fasta.eg', 'w') as writer:
-	writer.writelines("%s\n" % i for i in mylines)
+	with open("file"+str(x), 'w') as writer:
+ 		writer.writelines("%s\n" % i for i in mylines)
+ 		x += 1
 
-mylines = []
+# mylines = []
 
-with open('chrMchr1-2.fasta.eg', 'r') as myfile:
-	for a_line in myfile:
-		if a_line == '>2 chr1:1-X\n':
-			break
-		mylines.append(a_line.rstrip('\n'))
+# with open('chrMchr1-2.fasta.eg', 'r') as myfile:
+# 	for a_line in myfile:
+# 		if a_line == '>2 chr1:1-X\n':
+# 			break
+# 		mylines.append(a_line.rstrip('\n'))
 
-with open('chrMchr1_2_ONLY.fasta.eg', 'w') as writer:
-	writer.writelines("%s\n" % i for i in mylines)
-	
+# with open('chrMchr1_2_ONLY.fasta.eg', 'w') as writer:
+# 	writer.writelines("%s\n" % i for i in mylines)
+# 	
