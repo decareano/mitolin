a8ea8284c8e29694678d7cb39dc2a3c936ac1893
Marcelo Gobelli <marcelo@Marcelos-MacBook-Pro.local>
2019-07-28 14:42:49 -0700

    additional cleanup

--- F1_script_molly.js
+++
@@ -1,7 +0,0 @@
-var jq = document.createElement('script');
-jq.src = "https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js";
-document.getElementsByTagName('head')[0].appendChild(jq);
-jQuery.noConflict();
-
-
-$("#chart_div div div div table tbody tr td").map(function(){return $(this).text()})
